cc4fa27ed59f00ee52652473f71fd76217bb25af
Yato-kun <kirito0160218@gmail.com>
2024-06-01 22:36:41 -0700

    over_the_wire_function_ssh

--- things.sh
+++ things.sh
@@ -24,3 +24,27 @@ function cppss() {
     touch "$get_name" && chmod +x "$get_name"
   fi
 }
+
+# this function is uses to connect with ssh to overthewire.org
+# only write the number of the bandit for exaple = conover 1 "the results is bandit2". 
+# to connect with bandit 0 use only the name of the function example = conover
+function conover(){
+
+  argument=$1
+  bandit=$((argument + 1))
+  set_name="bandit${argument}@bandit.labs.overthewire.org"
+
+  if [[ $# -eq 0 ]]; then
+    echo "[+] Connecting to https://overthewire.org/wargames/bandit/bandit1.html"
+    ssh "bandit0@bandit.labs.overthewire.org" -p 2220
+  fi
+
+  if [ -n $1 ]; then
+
+    if [ $argument -ge 1 ]; then
+      echo "[+] Connecting to https://overthewire.org/wargames/bandit/bandit${bandit}.html"
+    fi
+    ssh "$set_name" -p 2220
+  fi
+
+}
