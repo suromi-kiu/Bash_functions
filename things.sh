c1c2366d7c97b2b95923ab8563b96c5b2b24fdf1
suromi <150649206+suromi-kiu@users.noreply.github.com>
2024-06-08 18:47:43 -0700

    Update things.sh

--- things.sh
+++ things.sh
@@ -1,4 +1,13 @@
-# here are some small bash functions for file creation
+function marpy() {
+  if [[ $# -eq 0 ]]; then
+    echo "Error: write a name"
+  fi
+
+  if [[ -n "$1" ]]; then
+    set_name="${1}.py"
+    touch "$set_name" && chmod +x "$set_name"
+  fi
+}
 
 # create a python file add x permits and write shebang
 function marpy() {
@@ -6,7 +15,7 @@ function marpy() {
     echo "Error: write a name"
   fi
 
-  if [ -n "$1" ]; then
+  if [[ -n "$1" ]]; then
     set_name="${1}.py"
     touch "$set_name" && chmod 754 "$set_name"
     echo "#!/usr/bin/env python3" > "$set_name"
@@ -19,7 +28,7 @@ function cppss() {
     echo "Error: write a name"
   fi
 
-  if [ -n "$1" ]; then
+  if [[ -n "$1" ]]; then
     get_name="${1}.cpp"
     touch "$get_name" && chmod 754 "$get_name"
   fi
@@ -27,14 +36,15 @@ function cppss() {
 
 # create a bash file add shebang and chmod 754
 function setbsh(){
-  if [ $# -eq 0 ]; then
+  if [[ $# -eq 0 ]]; then
     echo "Write a name bro"
+    return 1
   fi
 
-  if [ -n "$1"]; then
+  if [[ -n "$1" ]]; then
     get_name="${1}.sh"
-    touch "$get_name" && chmod 754 "$set_name"
-    echo "#!/usr/bin/env bash" > "$set_name"
+    touch "$get_name" && chmod 754 "$get_name"
+    echo "#!/usr/bin/env bash" > "$get_name"
   fi
 }
 
